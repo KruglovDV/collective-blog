3932017134446bcb8746a8ed31368bd4f98dbe60
dimakruglov <kruglovdmitrij@itclover.ru>
2022-07-18 20:10:36 +0300

    feat: highlight current herf in nav

--- collective_blog/app/javascript/packs/application.js
+++ collective_blog/app/javascript/packs/application.js
@@ -3,10 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import "bootstrap/dist/css/bootstrap.min.css"
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
+import "@fortawesome/fontawesome-free/css/all"
 
 Rails.start()
 Turbolinks.start()
