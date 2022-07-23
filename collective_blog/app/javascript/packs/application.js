beb7411c36964999af14d3d4e568dbe57bd92b3a
dimakruglov <kruglovdmitrij@itclover.ru>
2022-07-23 21:27:32 +0300

    feat: adds collapse for comment form

--- collective_blog/app/javascript/packs/application.js
+++ collective_blog/app/javascript/packs/application.js
@@ -9,6 +9,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+require("stylesheets/application.scss")
 import "@fortawesome/fontawesome-free/css/all"
 
 Rails.start()
