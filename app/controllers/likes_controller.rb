9ef6edcbe91f531e3ed14b99946b38330ef48569
dimakruglov <kruglovdmitrij@itclover.ru>
2022-08-04 10:54:47 +0300

    fix: counter cache usage, create like action and class names usage

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,13 +4,9 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    post_id = params[:post_id]
-    @post = Post.find(post_id)
-
-    unless @post.likes.find_by(post_id: post_id, user_id: current_user.id)
-      @like = @post.likes.build(user: current_user)
-      @like.save
-    end
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.find_or_initialize_by(user_id: current_user.id)
+    @like.save
     redirect_to @post
   end
 
